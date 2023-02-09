e723335018daf5092025b2525b542c6d313301ce
martynlodder <102233130+martynlodder@users.noreply.github.com>
2023-02-09 11:00:55 +0100

    GateKeeper V2 - Greets based on the time of day
    Welcome message based on the time of day.

--- GateKeeper.go
+++ GateKeeper.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func timeCheck() {
+	datetime := time.Now()  // Fetch the current date and time
+	time := datetime.Hour() // Fetch only the hour
+
+	if time > 7 && time < 12 {
+		fmt.Println("Goedemorgen! Welkom bij Fonteyn Vakantieparken!")
+	} else if time >= 12 && time < 17 {
+		fmt.Println("Goedemiddag! Welkom bij Fonteyn Vakantieparken!")
+	} else if time > 17 && time <= 23 {
+		fmt.Println("Goedenavond! Welkom bij Fonteyn Vakantieparken!")
+	} else {
+		fmt.Println("Sorry, de parkeerplaats is ’s nachts gesloten.")
+	}
+}
 
 func main() {
-	fmt.Println("Welkom bij Fonteyn Vakantieparken!")
+	timeCheck()
 }
